421796260d7984e5c5c01e509e43b1fade2cf8f9
Yameen <you@example.com>
2020-03-20 02:27:01 +0600

    Changed the images

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,28 +11,33 @@ class Directory extends Component {
       sections: [
         {
           title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          imageUrl:
+            "https://images.unsplash.com/photo-1534215754734-18e55d13e346?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
           id: 1
         },
         {
           title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          imageUrl:
+            "https://ae01.alicdn.com/kf/HTB1RQ8NLXXXXXXAapXXq6xXFXXXx/2016-2017-Latest-Hot-Selling-USA-justin-bieber-fear-of-god-hoodies-pullovers-hiphop-Fashion-Casual.jpg",
           id: 2
         },
         {
-          title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          title: "shoes",
+          imageUrl:
+            "https://ae01.alicdn.com/kf/HTB1aCEGBcyYBuNkSnfoq6AWgVXaZ/UBFEN-Hot-selling-fashion-Casual-Shoes-For-Men-comfortable-shoes-autumn-winter-warm-black-yellow-casual.jpg",
           id: 3
         },
         {
           title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/Womens.png",
+          imageUrl:
+            "https://i.pinimg.com/originals/60/e8/f3/60e8f3f3774269eee6b90fb40033fae3.jpg",
           size: "large",
           id: 4
         },
         {
           title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          imageUrl:
+            "https://i.pinimg.com/736x/bd/f8/12/bdf81227c8c13191aa249fa39384d144.jpg",
           size: "large",
           id: 5
         }
