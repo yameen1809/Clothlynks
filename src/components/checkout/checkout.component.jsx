5635149978d953216dc8c1b055bf652b30fdeba8
Yameen <you@example.com>
2020-03-23 17:58:42 +0600

    Updated stripe payment integration and the favicon

--- src/components/checkout/checkout.component.jsx
+++ src/components/checkout/checkout.component.jsx
@@ -10,6 +10,7 @@ import {
 } from "../../redux/cart/cart.selectors";
 
 import "./checkout.styles.scss";
+import StripeCheckoutButton from "../stripe-button/stripe-button.component";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -37,6 +38,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>Total : ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 04/20 - CVC: 123
+    </div>
+    <StripeCheckoutButton className="stripeButton" price={total} />
   </div>
 );
 
---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_tzj2KhAfDnf1FkiPW1WO6LVD00SjoZUXDG";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="E-Clothing"
+      billingAddress
+      shippingAddress
+      image="https://www.svgrepo.com/show/122129/clothes-hanging-on-rope-for-drying.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
