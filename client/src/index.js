9170e63ad2802747102982e37a09174ef4d97371
Yameen <you@example.com>
2020-03-28 23:11:37 +0600

    taking it back to where it is

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { store, persistor } from "./redux/store";
-import * as serviceWorker from "./serviceWorker";
 
 import "./index.css";
 import App from "./App";
@@ -20,5 +19,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-serviceWorker.register();
